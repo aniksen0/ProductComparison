69da754f918acfd1a65836f7566fe5bb2346d508
Anik Sen <37754808+aniksen0@users.noreply.github.com>
2023-04-07 01:09:20 +0600

    added headless browser and updated requirement.txt

--- utils/driver.py
+++ utils/driver.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 def initialize_driver():
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     # options.add_argument("--no-sandbox")
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--disable-notifications")
