304ec46cc0ae705c1742734193ad40b1abac082a
Anik Sen <37754808+aniksen0@users.noreply.github.com>
2023-04-07 02:23:41 +0600

    requirement txt changed

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import os
 
 command1 = "wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 command2 = "sudo dpkg -i google-chrome-stable_current_amd64.deb"
-
+command3 = "sudo apt-get install wget"
+os.system(command3)
 os.system(command1)
 os.system(command2)
